5736f9c2bc545626dd380c5e9b332f0629a695d8
Tam Pham <tam001p@gmail.com>
2025-03-13 12:55:30 -0500

    Add base monster schema, test importing monsters from SRD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+pub mod monster;
+
+pub use monster::Monster;
+
+/// A combatant in the initiative tracker.
+///
+/// Combatants can include player characters, monsters, NPCs, etc.
+pub enum Combatant {
+    /// Pre-made monster.
+    Monster(Monster),
+}
+
+/// The core initiative tracker.
+///
+/// It handles the order of play and tracks every important detail, such as the current turn,
+/// conditions on each combatant, actions taken, etc.
+pub struct Tracker {
+    /// The current turn.
+    pub turn: usize,
+
+    /// The current round, starting at 0 (to mean the first round).
+    pub round: usize,
+
+    /// The list of combatants.
+    pub combatants: Vec<Combatant>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // NOTE: monster JSON data provided courtesy of https://www.dnd5eapi.co/
+    let file = std::fs::File::open("data/monsters.json").unwrap();
+    let monsters = serde_json::from_reader::<_, Vec<Monster>>(file).unwrap();
+    println!("{:#?}", monsters);
 }
---
+++ src/monster.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+
+/// The ability scores of a creature.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AbilityScores {
+    pub strength: i32,
+    pub dexterity: i32,
+    pub constitution: i32,
+    pub intelligence: i32,
+    pub wisdom: i32,
+    pub charisma: i32,
+}
+
+/// A creature's size.
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Size {
+    Tiny,
+    Small,
+    Medium,
+    Large,
+    Huge,
+    Gargantuan,
+}
+
+/// A creature's type.
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Type {
+    Aberration,
+    Beast,
+    Celestial,
+    Construct,
+    Dragon,
+    Elemental,
+    Fey,
+    Fiend,
+    Giant,
+    Humanoid,
+    Monstrosity,
+    Ooze,
+    Plant,
+    Undead,
+
+    #[serde(other)] // TODO: capture the value of the "other" case
+    Other,
+}
+
+/// A pre-made monster from the System Reference Document (SRD), or a custom monster.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Monster {
+    /// The monster's index, used for identification.
+    pub index: String,
+
+    /// The monster's name.
+    pub name: String,
+
+    /// The monster's ability scores, used for calculating modifiers.
+    #[serde(flatten)]
+    pub scores: AbilityScores,
+
+    /// The monster's size, used to determine the amount of space it occupies on the battlefield.
+    pub size: Size,
+
+    /// The type of monster, used for categorization, or for spells and abilities that target
+    /// specific types of creatures.
+    pub r#type: Type,
+
+    /// The monster's armor class, the amount needed to hit it with an attack.
+    // pub armor_class: i32,
+
+    /// The monster's hit points.
+    pub hit_points: i32,
+}
