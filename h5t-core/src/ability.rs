941e5b23e58d48252a7f390bf521bb20f1412d70
Tam Pham <tam001p@gmail.com>
2026-05-31 00:25:48 -0500

    Move modificer_calculation test to ability.rs file

--- h5t-core/src/ability.rs
+++ h5t-core/src/ability.rs
@@ -99,3 +99,49 @@ pub struct Skill<T> {
     /// Survival (Wisdom).
     pub survival: T,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::score_to_modifier;
+
+    /// Ensure the modifier calculation is correct.
+    #[test]
+    fn modifier_calculation() {
+        let tests = [
+            (1, -5),
+            (2, -4),
+            (3, -4),
+            (4, -3),
+            (5, -3),
+            (6, -2),
+            (7, -2),
+            (8, -1),
+            (9, -1),
+            (10, 0),
+            (11, 0),
+            (12, 1),
+            (13, 1),
+            (14, 2),
+            (15, 2),
+            (16, 3),
+            (17, 3),
+            (18, 4),
+            (19, 4),
+            (20, 5),
+            (21, 5),
+            (22, 6),
+            (23, 6),
+            (24, 7),
+            (25, 7),
+            (26, 8),
+            (27, 8),
+            (28, 9),
+            (29, 9),
+            (30, 10),
+        ];
+
+        for (score, modifier) in tests.iter() {
+            assert_eq!(score_to_modifier(*score), *modifier);
+        }
+    }
+}
