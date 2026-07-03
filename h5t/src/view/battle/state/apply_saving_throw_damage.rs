76004d5299ddd81706d83a5a735142cb1bfae6c6
Tam Pham <tam001p@gmail.com>
2026-07-03 07:39:38 -0500

    Refactor massive functions in ApplySavingThrowDamage, fix weird field ordering

--- h5t/src/view/battle/state/apply_saving_throw_damage.rs
+++ h5t/src/view/battle/state/apply_saving_throw_damage.rs
@@ -30,7 +30,7 @@ fn fmt_dice_expr(modifier: i32) -> String {
 
 /// Helper enum to indicate which form field is currently selected.
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
-enum Field {
+enum Step {
     #[default]
     Ability,
     RollSave,
@@ -72,47 +72,185 @@ impl CombatantData {
 /// State for applying damage to combatants after requiring a saving throw.
 #[derive(Clone, Debug, Default)]
 pub struct ApplySavingThrowDamage {
-    /// Indicates which form field is currently selected.
-    field: Field,
+    /// Indicates which step of the state we're currently on.
+    step: Step,
 
     /// The combatant indices to apply damage to.
     combatants: Vec<CombatantData>,
+
+    /// The ability combatants must use for the saving throw.
+    ability: Option<AbilityKind>,
+
+    /// Helper to get the saving throw from the user.
+    saving_throw: GetInput<i32>,
 
     /// The combatant within the list of targeted combatants (`self.combatants`) we are rolling a
     /// saving throw for.
     selected_idx: usize,
 
-    /// Helper to get the saving throw from the user.
-    saving_throw: GetInput<i32>,
-
-    /// The ability combatants must use for the saving throw.
-    ability: Option<AbilityKind>,
+    /// Helper to get the damage value from the user.
+    damage_value: GetInput<i32>,
 
     /// The type of damage to apply. This can be omitted to simply reduce combatant HP without
     /// dealing with vulnerabilities, resistances, or immunities.
-    kind: Option<DamageKind>,
-
-    /// Helper to get input from the user.
-    input: GetInput<i32>,
+    damage_kind: Option<DamageKind>,
 }
 
 impl ApplySavingThrowDamage {
     /// Create an [`ApplySavingThrowDamage`] state with the given combatants.
     pub fn new(combatants: Vec<CombatantData>) -> Self {
         Self {
-            field: Field::default(),
+            step: Step::default(),
             combatants,
-            selected_idx: 0,
+            ability: None,
             saving_throw: GetInput::new("Saving throw", 3, Charset::Numeric)
                 .active(false),
-            ability: None,
-            kind: None,
-            input: GetInput::new("Damage amount", 4, Charset::Numeric) // damage is usually 1-2 digits
+            selected_idx: 0,
+            damage_value: GetInput::new("Damage amount", 4, Charset::Numeric) // damage is usually 1-2 digits
                 .active(false)
                 .suffix("HP"),
+            damage_kind: None,
         }
     }
 
+    /// Switch the step and the corresponding visual aids.
+    fn set_step(&mut self, new_step: Step) {
+        self.step = new_step;
+        self.saving_throw.set_active(new_step == Step::RollSave);
+        self.damage_value.set_active(new_step == Step::Damage);
+    }
+
+    /// Change the selected combatant in the saving throw table.
+    fn set_selected_idx(&mut self, new_idx: usize) {
+        self.selected_idx = new_idx;
+
+        let data = &self.combatants[new_idx];
+        let save_value = data.save
+            .map(|save| save.to_string())
+            .unwrap_or(String::new());
+        let save_mod = self.ability
+            .map(|ability| save_modifier(ability, data.proficiencies, data.modifiers))
+            .unwrap_or(0);
+        self.saving_throw.set_value(save_value);
+        self.saving_throw.set_prefix(fmt_dice_expr(save_mod));
+    }
+
+    /// Handle a key event during the [`Field::Ability`] step.
+    fn ability_step(&mut self, key: KeyEvent) -> AfterKey {
+        let label_to_option = LABELS
+            .into_iter()
+            .zip(AbilityKind::owned_variants())
+            .collect::<HashMap<_, _>>();
+
+        match key.code {
+            KeyCode::Esc => return AfterKey::Exit,
+            KeyCode::Enter => self.set_step(Step::RollSave),
+            KeyCode::Char(label) => {
+                if let Some(&option) = label_to_option.get(&label) {
+                    if self.ability == Some(option) {
+                        self.ability = None;
+                    } else {
+                        self.ability = Some(option);
+                    }
+
+                    // set value and prefix of saving throw input
+                    self.set_selected_idx(self.selected_idx);
+                }
+            },
+            _ => (),
+        }
+
+        AfterKey::Stay
+    }
+
+    /// Handle a key event during the [`Field::RollSave`] step.
+    fn roll_save_step(&mut self, key: KeyEvent) -> AfterKey {
+        match self.saving_throw.handle_key(key) {
+            AfterKeyInner::Handled => {
+                let Ok(save) = self.saving_throw.get_parsed() else {
+                    return AfterKey::Stay;
+                };
+
+                let combatant = &mut self.combatants[self.selected_idx];
+                combatant.save = Some(save);
+            },
+            AfterKeyInner::Submit(save) => {
+                let combatant = &mut self.combatants[self.selected_idx];
+                combatant.save = Some(save);
+
+                // if all combatants have a saving throw, move on to the damage field
+                if self.combatants.iter().all(|data| data.save.is_some()) {
+                    self.set_step(Step::Damage);
+                } else {
+                    // move to next combatant with no saving throw set
+                    let mut next_idx = self.selected_idx;
+                    loop {
+                        next_idx = (next_idx + 1) % self.combatants.len();
+                        if self.combatants[next_idx].save.is_none() {
+                            self.set_selected_idx(next_idx);
+                            break;
+                        }
+                    }
+                }
+            },
+            AfterKeyInner::Cancel => self.set_step(Step::Ability),
+            AfterKeyInner::Forward(key) => {
+                let KeyCode::Char(label) = key.code else {
+                    return AfterKey::Stay;
+                };
+
+                let Some(idx) = LABELS.into_iter().position(|ch| ch == label) else {
+                    return AfterKey::Stay;
+                };
+
+                if idx < self.combatants.len() {
+                    self.set_selected_idx(idx);
+                }
+            },
+        }
+
+        AfterKey::Stay
+    }
+
+    /// Handle a key event during the [`Field::Damage`] step and apply any needed changes to the
+    /// tracker.
+    fn damage_step(&mut self, key: KeyEvent, tracker: &mut Tracker) -> AfterKey {
+        match self.damage_value.handle_key(key) {
+            AfterKeyInner::Handled => (),
+            AfterKeyInner::Submit(value) => {
+                for data in &self.combatants {
+                    let combatant_idx = data.idx;
+                    let combatant = &mut tracker.combatants[combatant_idx];
+                    combatant.damage(value, self.damage_kind);
+                }
+                return AfterKey::Exit;
+            },
+            AfterKeyInner::Cancel => self.set_step(Step::RollSave),
+            AfterKeyInner::Forward(key) => {
+                let KeyCode::Char(label) = key.code else {
+                    return AfterKey::Stay;
+                };
+
+                let label_to_option = LABELS
+                    .into_iter()
+                    .zip(DamageKind::owned_variants())
+                    .collect::<HashMap<_, _>>();
+
+                if let Some(&option) = label_to_option.get(&label) {
+                    if self.damage_kind == Some(option) {
+                        self.damage_kind = None;
+                    } else {
+                        self.damage_kind = Some(option);
+                    }
+                }
+            },
+        }
+
+        AfterKey::Stay
+    }
+}
+
+impl ApplySavingThrowDamage {
     /// Draw the state to the given [`Frame`].
     pub fn draw(&self, frame: &mut Frame) {
         let [select_ability, saving_throws, damage] = Layout::horizontal([
@@ -138,10 +276,10 @@ impl ApplySavingThrowDamage {
         frame.render_widget(Select::with_enum(
             "Select save ability",
             self.ability.as_ref(),
-            self.field == Field::Ability,
+            self.step == Step::Ability,
         ), select_ability);
 
-        let roll_save_theme = if self.field == Field::RollSave {
+        let roll_save_theme = if self.step == Step::RollSave {
             THEME
         } else {
             THEME.dim()
@@ -196,157 +334,25 @@ impl ApplySavingThrowDamage {
         let popup = Popup::new(
             THEME.foreground,
             Some("Roll saving throws"),
-            self.field == Field::RollSave,
+            self.step == Step::RollSave,
             table,
         );
         popup.render(save_table, frame.buffer_mut());
 
-        self.input.draw(frame, damage_amount);
+        self.damage_value.draw(frame, damage_amount);
         frame.render_widget(Select::with_enum(
             "Select damage type",
-            self.kind.as_ref(),
-            self.field == Field::Damage,
+            self.damage_kind.as_ref(),
+            self.step == Step::Damage,
         ), damage_kind);
     }
 
     /// Handle a key event and apply any needed changes to the tracker.
     pub fn handle_key(&mut self, key: KeyEvent, tracker: &mut Tracker) -> AfterKey {
-        if self.field == Field::Ability {
-            let label_to_option = LABELS
-                .into_iter()
-                .zip(AbilityKind::owned_variants())
-                .collect::<HashMap<_, _>>();
-
-            match key.code {
-                KeyCode::Esc => return AfterKey::Exit,
-                KeyCode::Enter => {
-                    self.field = Field::RollSave;
-                    self.saving_throw.set_active(true);
-                },
-                KeyCode::Char(label) => {
-                    if let Some(&option) = label_to_option.get(&label) {
-                        if self.ability == Some(option) {
-                            self.ability = None;
-                            self.saving_throw.set_prefix("");
-                        } else {
-                            self.ability = Some(option);
-
-                            let data = &self.combatants[self.selected_idx];
-                            let save_mod = self.ability
-                                .map(|ability| save_modifier(ability, data.proficiencies, data.modifiers))
-                                .unwrap_or(0);
-                            self.saving_throw.set_prefix(fmt_dice_expr(save_mod));
-                        }
-                    }
-                },
-                _ => (),
-            }
-
-            AfterKey::Stay
-        } else if self.field == Field::RollSave {
-            match self.saving_throw.handle_key(key) {
-                AfterKeyInner::Handled => {
-                    let Ok(save) = self.saving_throw.get_parsed() else {
-                        return AfterKey::Stay;
-                    };
-
-                    let combatant = &mut self.combatants[self.selected_idx];
-                    combatant.save = Some(save);
-                },
-                AfterKeyInner::Submit(save) => {
-                    let combatant = &mut self.combatants[self.selected_idx];
-                    combatant.save = Some(save);
-
-                    // if all combatants have a saving throw, move on to the damage field
-                    if self.combatants.iter().all(|data| data.save.is_some()) {
-                        self.field = Field::Damage;
-                        self.saving_throw.set_active(false);
-                        self.input.set_active(true);
-                    } else {
-                        // move to next combatant with no saving throw set
-                        let mut next_idx = self.selected_idx;
-                        loop {
-                            next_idx = (next_idx + 1) % self.combatants.len();
-                            if self.combatants[next_idx].save.is_none() {
-                                self.set_selected_idx(next_idx);
-                                break;
-                            }
-                        }
-                    }
-                },
-                AfterKeyInner::Cancel => {
-                    self.field = Field::Ability;
-                    self.saving_throw.set_active(false);
-                },
-                AfterKeyInner::Forward(key) => {
-                    let KeyCode::Char(label) = key.code else {
-                        return AfterKey::Stay;
-                    };
-
-                    let Some(idx) = LABELS.into_iter().position(|ch| ch == label) else {
-                        return AfterKey::Stay;
-                    };
-
-                    if idx < self.combatants.len() {
-                        self.set_selected_idx(idx);
-                    }
-                },
-            }
-
-            AfterKey::Stay
-        } else {
-            match self.input.handle_key(key) {
-                AfterKeyInner::Handled => AfterKey::Stay,
-                AfterKeyInner::Submit(value) => {
-                    for data in &self.combatants {
-                        let combatant_idx = data.idx;
-                        let combatant = &mut tracker.combatants[combatant_idx];
-                        combatant.damage(value, self.kind);
-                    }
-                    AfterKey::Exit
-                },
-                AfterKeyInner::Cancel => {
-                    self.field = Field::RollSave;
-                    self.saving_throw.set_active(true);
-                    self.input.set_active(false);
-                    AfterKey::Stay
-                },
-                AfterKeyInner::Forward(key) => {
-                    let KeyCode::Char(label) = key.code else {
-                        return AfterKey::Stay;
-                    };
-
-                    let label_to_option = LABELS
-                        .into_iter()
-                        .zip(DamageKind::owned_variants())
-                        .collect::<HashMap<_, _>>();
-
-                    if let Some(&option) = label_to_option.get(&label) {
-                        if self.kind == Some(option) {
-                            self.kind = None;
-                        } else {
-                            self.kind = Some(option);
-                        }
-                    }
-
-                    AfterKey::Stay
-                },
-            }
+        match self.step {
+            Step::Ability => self.ability_step(key),
+            Step::RollSave => self.roll_save_step(key),
+            Step::Damage => self.damage_step(key, tracker),
         }
-    }
-
-    /// Change the selected combatant in the saving throw table.
-    fn set_selected_idx(&mut self, new_idx: usize) {
-        self.selected_idx = new_idx;
-
-        let data = &self.combatants[new_idx];
-        let save_value = data.save
-            .map(|save| save.to_string())
-            .unwrap_or(String::new());
-        let save_mod = self.ability
-            .map(|ability| save_modifier(ability, data.proficiencies, data.modifiers))
-            .unwrap_or(0);
-        self.saving_throw.set_value(save_value);
-        self.saving_throw.set_prefix(fmt_dice_expr(save_mod));
     }
 }
